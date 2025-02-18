89f49107d9299aaa28bd0f84b26ce49fd273c07c
urimeee <gimyulim375@gmail.com>
2025-02-18 07:17:59 +0000

    feat: bottom navigation 스타일 수정 / 13385231910

--- src/components/BottomNavi/BottomNavi.style.ts
+++ src/components/BottomNavi/BottomNavi.style.ts
@@ -6,9 +6,9 @@ export const StyledLink = styled(Link)`
 `;
 
 export const BottomNavWrapper = styled.div`
+  width: 100%;
   position: fixed;
-  left: 0;
-  right: 0;
+  bottom: 0;
   display: flex;
   justify-content: space-around;
   align-items: center;
