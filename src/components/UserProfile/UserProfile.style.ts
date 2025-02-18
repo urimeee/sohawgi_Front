72572500fd800b4554f5f28bf30260b1b5f81eda
urimeee <gimyulim375@gmail.com>
2025-02-18 07:09:40 +0000

    feat: 더보기 페이지 padding top 삭제 / 13385115674

--- src/components/UserProfile/UserProfile.style.ts
+++ src/components/UserProfile/UserProfile.style.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
-  padding-top: 4.875rem;
   padding-bottom: 3.75rem;
   gap: 1.25rem;
 `;
