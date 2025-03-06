c0ca50b36981312952015f988465ff83eae635ad
urimeee <gimyulim375@gmail.com>
2025-03-06 14:39:52 +0000

    feat: pxr 적용 / 13701192047

--- src/components/ScheduleDetail/ScheduleDetail.tsx
+++ src/components/ScheduleDetail/ScheduleDetail.tsx
@@ -15,7 +15,7 @@ const ScheduleDetail = ({ month, day, dayOfWeek, title, onClick }: Props) => {
       className="flex items-center justify-start gap-[0.9rem] w-full cursor-pointer"
       onClick={onClick}
     >
-      <img src={hamtori} alt="Hamtori" className="w-10 h-10" />
+      <img src={hamtori} alt="Hamtori" className="w-40 h-40" />
       <div className="flex flex-col gap-[0.05rem]">
         <div className="text-Grey_05 body_05">
           {month}월 {day}일 ({dayOfWeek})
