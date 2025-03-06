75108b428cdc3602cba2c45addd74325f03daf22
urimeee <gimyulim375@gmail.com>
2025-03-06 17:02:35 +0000

    feat: input field 수정 / 13704077902

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -17,7 +17,7 @@ const TextField: React.FC<TextFieldProps> = ({
     <form className={'relative flex w-full'} onSubmit={postSchedule}>
       <input
         className={
-          'w-full px-[1.0625rem] py-[0.8125rem] pr-[3.6rem] outline-none text-Grey_06 body_03 bg-Grey_02 rounded-10 focus:outline-Grey_06 placeholder:text-Grey_04 placeholder:text-14'
+          'w-full px-[1.0625rem] py-[0.8125rem] pr-[3.6rem] outline-none text-Grey_06 body_03 bg-Grey_02 rounded-10 focus:border-1 focus:border-Grey_06 placeholder:text-Grey_04 placeholder:text-14'
         }
         type="text"
         placeholder="일정을 입력하세요"
