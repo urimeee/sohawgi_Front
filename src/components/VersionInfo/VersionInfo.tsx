9ce0883aeeb9c34e82537f879f9974c3963dea09
urimeee <gimyulim375@gmail.com>
2025-03-11 17:24:11 +0000

    버전 정보 1.1.0 수정 / 13794292405

--- src/components/VersionInfo/VersionInfo.tsx
+++ src/components/VersionInfo/VersionInfo.tsx
@@ -2,7 +2,7 @@ const VersionInfo = () => {
   return (
     <div className={'flex pt-27 justify-between text-Grey_04 '}>
       <p>버전정보</p>
-      <p>1.0.0</p>
+      <p>1.1.0</p>
     </div>
   );
 };
