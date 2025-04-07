46a670054b4d259144eb9819cbf74a6ea262bfdb
urimeee <gimyulim375@gmail.com>
2025-04-07 14:35:04 +0000

    fix: url 수정 / 14312001972

--- src/hooks/useScheduleListQuery.ts
+++ src/hooks/useScheduleListQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { api } from '../utils/axios';
 
 const getSchedules = async () => {
-  const response = await api.get('/api/schedules');
+  const response = await api.get('/schedules');
   return response.data;
 };
 
