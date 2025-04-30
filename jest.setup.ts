0114de579be79369b4eb65c617c34747c7504245
urimeee <gimyulim375@gmail.com>
2025-04-30 16:09:46 +0000

    feat: rounded 값 변경 / 14759130262

--- jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-
-import dotenv from 'dotenv';
-import path from 'path';
-
-// 여기 추가
-dotenv.config({ path: path.resolve(__dirname, '../.env.test') });
--- src/pages/SchedulePage/SchedulePage.test.tsx
+++ src/pages/SchedulePage/SchedulePage.test.tsx
@@ -1,72 +1,11 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 import SchedulePage from './SchedulePage';
-import useSchedules from '../../hooks/useSchedule';
 
-jest.mock('../../hooks/useSchedule', () => ({
-  __esModule: true,
-  default: jest.fn(),
-}));
-
-jest.mock('../../utils/axios', () => {
-  return {
-    api: {
-      get: jest.fn(),
-      post: jest.fn(),
-      delete: jest.fn(),
-      interceptors: {
-        request: { use: jest.fn() },
-        response: { use: jest.fn() },
-      },
-    },
-  };
-});
-
-const mockedUseSchedules = useSchedules as jest.MockedFunction<
-  typeof useSchedules
->;
-
-describe('SchedulePage', () => {
-  const queryClient = new QueryClient();
-  const mockPostSchedule = jest.fn();
-
-  const renderWithProviders = (component: React.ReactNode) => {
-    return render(
-      <QueryClientProvider client={queryClient}>
-        {component}
-      </QueryClientProvider>,
-    );
-  };
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-    mockedUseSchedules.mockReturnValue({
-      postSchedule: mockPostSchedule,
-      deleteSchedule: jest.fn(),
-    });
-  });
-
-  it('스케줄 페이지 랜더링 시 TextField의 PlaceHolder가 보여야한다.', () => {
-    renderWithProviders(<SchedulePage />);
-    expect(
-      screen.getByPlaceholderText('예 ) 오늘 오후 7시 팀플 회의'),
-    ).toBeInTheDocument();
-  });
-
-  it('버튼 클릭시 일정이 등록되어야 한다', () => {
-    renderWithProviders(<SchedulePage />);
-    const inputElement =
-      screen.getByPlaceholderText('예 ) 오늘 오후 7시 팀플 회의');
-    const submitButton = screen.getByText('등록');
-
-    fireEvent.click(submitButton);
-    expect(mockPostSchedule).not.toHaveBeenCalled();
-
-    fireEvent.change(inputElement, {
-      target: { value: '오늘 오후 7시 팀플 회의' },
-    });
-    fireEvent.click(submitButton);
-    expect(mockPostSchedule).toHaveBeenCalledWith('오늘 오후 7시 팀플 회의');
-  });
+test('placeholder가 보인다', () => {
+  render(<SchedulePage />);
+  expect(
+    screen.getByPlaceholderText('예 ) 오늘 오후 7시 팀플 회의'),
+  ).toBeInTheDocument();
 });
