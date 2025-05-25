082135bc2a4ccb275f16119c8e55b19c6a8e3abc
urimeee <gimyulim375@gmail.com>
2025-05-25 17:29:19 +0000

    feat: 시간 형태 변환 로직 추가 / 15240302107

--- src/components/ScheduleDetail/ScheduleDetail.tsx
+++ src/components/ScheduleDetail/ScheduleDetail.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { formatTime } from '../../utils';
+
 import hamtori from '../../assets/images/hamtori.svg';
 
 type Props = {
@@ -16,7 +18,7 @@ const ScheduleDetail = ({ time, title, onClick }: Props) => {
       <img src={hamtori} alt="Hamtori" className="w-40 h-40" />
       <div className="flex flex-col gap-[0.05rem]">
         <div className="text-Grey_05 body_05">
-          {time}
+          {formatTime(time)}
         </div>
         <div className="text-Grey_06 body_02">{title}</div>
       </div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,11 @@ import dayjs from 'dayjs';
 export const convertDateFormat = (dateString: string) => {
   return dayjs(dateString).format('YYYY-MM-DD');
 };
+
+export const formatTime=(time: string) => {
+  const match = time.match(/(오전|오후)\s(\d{1,2})시\s(\d{2})분/);
+  if (!match) return time; // 패턴 안 맞으면 그대로 반환
+
+  const [, period, hour, minute] = match;
+  return `${period} ${hour}:${minute}`;
+}
