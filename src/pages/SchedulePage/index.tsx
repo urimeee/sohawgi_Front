85ab2c8ae566cbd247739236117ea217e3f7792b
urimeee <gimyulim375@gmail.com>
2025-06-01 16:23:38 +0000

    feat: 스크롤 영역 분리 / 15377089232

--- src/pages/SchedulePage/index.tsx
+++ src/pages/SchedulePage/index.tsx
@@ -14,19 +14,21 @@ const SchedulePage = () => {
   const [endDate, setEndDate] = useState<Dayjs>(dayjs().endOf('week'));
 
   return (
-    <div className="flex w-full flex-col px-18 h-screen no-scrollbar">
-      <div>
-        <TextField postSchedule={postSchedule} />
+    <div className="flex w-full flex-col px-18 h-screen no-scrollbar ">
+      <div className="sticky bg-white top-0 z-9">
+        <div>
+          <TextField postSchedule={postSchedule} />
+        </div>
+        <div>
+          <Calendar
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+          />
+        </div>
       </div>
-      <div>
-        <Calendar
-          selectedDate={selectedDate}
-          setSelectedDate={setSelectedDate}
-          setStartDate={setStartDate}
-          setEndDate={setEndDate}
-        />
-      </div>
-      <div className="flex-1 h-full">
+      <div className="flex-1 h-full overflow-y-auto">
         <ScheduleCard selectedDate = {selectedDate} />
       </div>
     </div>
